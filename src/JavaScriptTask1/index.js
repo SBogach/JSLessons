258d1400247ce2f302f363958c7fd3a43dbc682a
S.Bogach <S.Bogach@compassplus.com>
2025-05-26 17:21:12 +0500

    Comm2

---
+++ src/JavaScriptTask1/index.js
@@ -0,0 +1,19 @@
+//let answer = fetch('https://jsonplaceholder.typicode.com/posts')
+//                  .then(response => response.json())
+//                   .then(json => console.log(json));
+//console.log(answer);
+
+let answer = "[ { \"userId\": 1, \"id\": 1, \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\", \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"},  { \"userId\": 1, \"id\": 2, \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\", \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"}, { \"userId\": 1, \"id\": 3, \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\", \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"}]";
+console.log(answer);
+let parsedAnswer = JSON.parse(answer);
+console.log(parsedAnswer);
+
+let main = document.getElementById("mainElement");
+for (let i = 0; i <= 2; i++) {
+    let div = document.createElement("div");
+    div.className="topic";
+    div.innerHTML = `<img class="topicImage" src="https://placehold.co/300x200"></img>`;
+    div.innerHTML += `<h4 class="topicTitle">${parsedAnswer[i].title}</h4>`;
+    div.innerHTML += `<p class="topicText">${parsedAnswer[i].body}</p>`;
+    main.appendChild(div);
+}
